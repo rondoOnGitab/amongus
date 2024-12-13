26a2b484488a16bc972882d0b592f6eadb08d4c9
JEANMONNET\zanetti_gabriele <gabrielezanetti.gabriele@gmail.com>
2024-12-13 11:38:21 +0100

    struttura run del Thread coordinatesHandler

--- Server/CoordinatesHandler.java
+++ Server/CoordinatesHandler.java
@@ -1,10 +1,13 @@
 package Server;
 
+import Classes.*;
 import Client.GUIHandler;
+import java.util.ArrayList;
 
 public class CoordinatesHandler extends Thread
 {
     GUIHandler gh;
+    ArrayList<Entity> players;
     public CoordinatesHandler(GUIHandler gh)
     {
         this.gh = gh;
@@ -16,6 +19,10 @@ public class CoordinatesHandler extends Thread
         {
             //Controlla le coordinate di player X le manda al GUIHandler.java
             // per sapere il player scorro la lista di player controllo dalla socket l'ip e la port per capire se è il personaggio corretto
+            for (Entity e : this.players)
+            {
+                
+            }
         }
     }
 }
