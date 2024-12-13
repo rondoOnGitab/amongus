5f1cde954c912261c0b6df767e38c84bcac83caa
JEANMONNET\ronconi_alessandro <alessandroronconi4@gmail.com>
2024-12-13 10:35:00 +0100

    prove client

--- Client/App.java
+++ Client/App.java
@@ -1,7 +1,32 @@
 package Client;
 
-import Classes.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
 
-public class App {
-    
+class Client{
+public static void main(String[] args) throws IOException
+    {
+        
+        DatagramSocket socket = new DatagramSocket();
+
+        Scanner sc = new Scanner(System.in);
+        String mex = sc.nextLine();
+        String ip = "172.16.102.112";
+
+        byte[] buffer = mex.getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        packet.setAddress(InetAddress.getByName(ip));
+        packet.setPort(12345);
+        socket.send(packet);
+
+        byte[] bufferRisp = new byte[1500]; //1500 per MTU (cerca cos'è)
+        DatagramPacket packetRisp = new DatagramPacket(bufferRisp, bufferRisp.length);
+        socket.receive(packetRisp);
+        String mexRisp = new String(packetRisp.getData(),0,packetRisp.getLength());
+        System.out.println(mexRisp);
+        
+    }
 }
