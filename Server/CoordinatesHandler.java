48da3a318b88469444174def48488b813f9f3125
JEANMONNET\zanetti_gabriele <gabrielezanetti.gabriele@gmail.com>
2024-12-13 11:34:50 +0100

    aggiornamento commento per CooridnatesHandler

--- Server/CoordinatesHandler.java
+++ Server/CoordinatesHandler.java
@@ -15,6 +15,7 @@ public class CoordinatesHandler extends Thread
         while(true)
         {
             //Controlla le coordinate di player X le manda al GUIHandler.java
+            // per sapere il player scorro la lista di player controllo dalla socket l'ip e la port per capire se è il personaggio corretto
         }
     }
 }
